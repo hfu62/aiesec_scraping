513d5e4820edc3d5c067ee31443c51293c41f99b
Hiroko Fukushima <hiroko@Hiroko-no-MacBook-Air.local>
2016-01-29 05:58:00 +0900

    urlスクレイピングメソッド追加

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,5 +1,11 @@
 Class Scraping
   def self.tn_urls
+    links = []
+    agent = Mechanize.new
+    current_page = agent.get("https://opportunities.aiesec.org/programmes/GIP")
+    elements = current_page.serach('.info h1 a')
+    elements.each do |ele|
+      links << ele.get_attribute('href')
   end
 
   def self.tn_names
